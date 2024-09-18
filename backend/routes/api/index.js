186be6706e557cfe1114c2e447f760bd9b761f48
Jake Mann <JakeMann.com@gmail.com>
2024-09-18 13:56:49 -0700

    working token setter

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,8 +1,22 @@
 // backend/routes/api/index.js
 const router = require("express").Router();
+const { setTokenCookie } = require('../../utils/auth.js');
+const { User } = require('../../db/models');
+
 
 router.post("/test", function (req, res) {
   res.json({ requestBody: req.body });
 });
+
+router.get('/set-token-cookie', async (_req, res) => {
+  const user = await User.findOne({
+    where: {
+      username: 'Demo-lition'
+    }
+  });
+  setTokenCookie(res, user);
+  return res.json({ user: user });
+});
+
 
 module.exports = router;
