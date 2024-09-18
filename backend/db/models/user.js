b23c3922f0d929fc6cc60a7ce3422e83f2e4fab2
Jake Mann <JakeMann.com@gmail.com>
2024-09-18 13:33:45 -0700

    working seed/migrations/model

--- backend/db/models/user.js
+++ backend/db/models/user.js
@@ -1,16 +1,15 @@
-"use strict";
-const { Model } = require("sequelize");
+'use strict';
+
+const { Model, Validator } = require('sequelize');
+
 module.exports = (sequelize, DataTypes) => {
   class User extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
     static associate(models) {
       // define association here
     }
   }
+
+
   User.init(
     {
       username: {
@@ -20,8 +19,8 @@ module.exports = (sequelize, DataTypes) => {
         validate: {
           len: [4, 30],
           isNotEmail(value) {
-            if (validator.isEmail(value)) {
-              throw new Error("Cannot be an email.");
+            if (Validator.isEmail(value)) {
+              throw new Error('Cannot be an email.');
             }
           },
         },
@@ -45,7 +44,7 @@ module.exports = (sequelize, DataTypes) => {
     },
     {
       sequelize,
-      modelName: "User",
+      modelName: 'User',
     }
   );
   return User;
