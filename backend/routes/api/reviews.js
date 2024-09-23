e278879ed8588615dd600c895f798048f22b9281
Steven <stevenglab@gmail.com>
2024-09-23 11:25:03 -0700

    review model works and can get reviews + get reviews by spotId

--- backend/routes/api/reviews.js
+++ backend/routes/api/reviews.js
@@ -3,27 +3,21 @@ const express = require("express");
 const { Op, QueryInterface } = require("sequelize");
 const bcrypt = require("bcryptjs");
 const { setTokenCookie, restoreUser } = require("../../utils/auth");
-const { Spot, Review } = require("../../db/models");
-
+const { Review } = require("../../db/models");
 const app = require("../../app");
 const router = express.Router();
 const { check } = require("express-validator");
 const { handleValidationErrors } = require("../../utils/validation");
 
-router.get("/:spotId/reviews", async (req, res) => {
-  const spotId = req.params.spotId;
-  console.log("spotId", spotId);
-  const spotIdReviews = await Review.findAll({
+router.get("/current", async (req, res) => {
+  const { user } = req;
+  console.log("userID:", user.id);
+  const ourReviews = await Review.findAll({
     where: {
-      spotId,
+      userId: user.id,
     },
   });
-  res.status(200).json(spotIdReviews);
-});
-
-router.get("/", async (req, res) => {
-  const allSpots = await Spot.findAll();
-  res.json(allSpots);
+  res.status(200).json(ourReviews);
 });
 
 module.exports = router;
