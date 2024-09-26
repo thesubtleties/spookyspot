9ff36bbbc6e2f53677f1446a9239dc0e5009ba10
Jake Mann <JakeMann.com@gmail.com>
2024-09-26 08:32:36 -0600

    added onDelete to spot model (Marc said postgres requires this... something about children)

--- backend/db/models/spot.js
+++ backend/db/models/spot.js
@@ -12,7 +12,10 @@ module.exports = (sequelize, DataTypes) => {
     static associate(models) {
       // define association here
 
-      Spot.hasMany(models.Booking, { foreignKey: "spotId" });
+      Spot.hasMany(models.Booking, { 
+        foreignKey: "spotId",
+        onDelete: "CASCADE",
+      });
       Spot.hasMany(models.Review, {
         foreignKey: "spotId",
         onDelete: "CASCADE",
