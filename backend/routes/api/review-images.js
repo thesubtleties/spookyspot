ad605362f51549de8b0aa53b90af7ae12f40bbe0
Steven <stevenglab@gmail.com>
2024-09-26 17:07:49 -0700

    updated typing on review image, removed url constraint on review image, removed unique constraint on review image

--- backend/routes/api/review-images.js
+++ backend/routes/api/review-images.js
@@ -4,36 +4,31 @@ const { Review, ReviewImage } = require("../../db/models");
 const router = express.Router();
 const { requireAuth } = require("../../utils/auth");
 
-
-
 // Delete a Review Image
-router.delete('/:imageId', requireAuth, async (req, res) => {
-  const { imageId } = req.params;
+router.delete("/:imageId", requireAuth, async (req, res) => {
+  const imageId = req.params.imageId;
   const userId = req.user.id;
 
-    const reviewImage = await ReviewImage.findByPk(imageId, {
-      include: {
-        model: Review,
-        attributes: ['userId']
-      }
-    });
+  const reviewImage = await ReviewImage.findByPk(imageId, {
+    include: {
+      model: Review,
+      attributes: ["userId"],
+    },
+  });
+  console.log(reviewImage);
 
-    if (!reviewImage) {
-      return res.status(404).json({ message: "Review Image couldn't be found" });
-    }
+  if (!reviewImage) {
+    return res.status(404).json({ message: "Review Image couldn't be found" });
+  }
 
-    if (reviewImage.Review.userId !== userId) {
-      return res.status(403).json({ message: "Forbidden" });
-    }
+  if (reviewImage.Review.userId !== userId) {
+    return res.status(403).json({ message: "Forbidden" });
+  }
 
-    await ReviewImage.destroy({
-      where: {
-        id: imageId,
-      },
-    });
-
-    res.status(200).json({ message: "Successfully deleted" });
+  await reviewImage.destroy();
+  console.log(res);
+  console.log(res.body);
+  res.status(200).json({ message: "Successfully deleted" });
 });
-
 
 module.exports = router;
