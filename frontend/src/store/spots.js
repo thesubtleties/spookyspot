fc68bbc04dc677a3bd45f79ccb85377fa2595612
Steven <stevenglab@gmail.com>
2024-10-18 21:54:08 -0700

    working on spots ducks file

--- frontend/src/store/spots.js
+++ frontend/src/store/spots.js
@@ -1,7 +1,47 @@
+import { TbAirConditioning } from 'react-icons/tb';
 import { csrfFetch } from './csrf';
 
-const FETCH_SPOTS_SUCCESS = '/spots';
+const GET_ALL_SPOTS = 'spots/getAllSpots';
+const ADD_SPOT = 'spots/addSpot';
+const UPDATE_SPOT = 'spots/updateSpot';
+const DELETE_SPOT = 'spots/deleteSpot';
 
+export const getAllSpots = (spots) => ({
+  type: GET_ALL_SPOTS,
+  payload: spots,
+});
+
+export const fetchAllSpots = () => async (dispatch) => {
+  try {
+    const response = await csrfFetch('/spots');
+    if (!response.ok) throw new Error('Failed to fetch spots');
+    const spots = await response.json();
+    dispatch(getAllSpots(spots));
+  } catch (error) {}
+};
 const initialState = {
   allSpots: [],
+  currentSpot: null,
 };
+
+function spotReducer(state = initialState, action) {
+  switch (action.type) {
+    case 'GET_ALL_SPOTS':
+      return {
+        ...state,
+        allSpots: action.payload,
+      };
+    case 'ADD_SPOT':
+      return {
+        ...state,
+        allSpots: [...state.allSpots, action.payload],
+      };
+    case 'UPDATE_SPOT':
+      return {
+        ...state,
+        allSpots: state.allSpots.map((spot) =>
+          spot.id === action.payload.id ? action.payload : spot
+        ),
+      };
+  }
+}
